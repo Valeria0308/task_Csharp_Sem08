b38ce1db701b69fe0b15d4b4f3536b37384ae3bf
Valeria <133519509+Valeria0308@users.noreply.github.com>
2023-07-12 20:37:29 +0300

    added task02

---
+++ task02/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void Main()
+{
+    int rows = ReadInt("Введите количество строк: ");
+    int cols = ReadInt("Ведите количество стобцов: ");
+    int[,] matrix = FillMatrix(rows, cols, 0, 10);
+    PrintMatrix(matrix);
+    int row = RowMinSumElements(matrix);
+    System.Console.WriteLine($"Наименьшая сумма элементов в {row} строке.");
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int RowMinSumElements(int[,] matr)
+{
+    int[] arrSum = new int[matr.GetLength(0)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            arrSum[i] += matr[i, j];
+        }
+    }
+    int sum = arrSum[0];
+    int row = 1;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        if (arrSum[i] < sum)
+        {
+            sum = arrSum[i];
+            row = i + 1;
+        }
+    }
+    return row;
+}
+
+Main();
