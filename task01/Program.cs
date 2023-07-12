6c0068ada3ff70b2d976fb13dc739fa534447018
Valeria <133519509+Valeria0308@users.noreply.github.com>
2023-07-12 20:34:41 +0300

    added task01

---
+++ task01/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void Main()
+{
+    int rows = ReadInt("Введите количество строк: ");
+    int cols = ReadInt("Ведите количество стобцов: ");
+    int[,] matrix = FillMatrix(rows, cols, 0, 100);
+    PrintMatrix(matrix);
+    Streamline(matrix);
+    System.Console.WriteLine();
+    System.Console.WriteLine();
+    PrintMatrix(matrix);
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] Streamline(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int temp = 0; temp < matrix.GetLength(1) - 1; temp++)
+            {
+                if (matrix[i, temp] < matrix[i, temp + 1])
+                {
+                    (matrix[i, temp], matrix[i, temp + 1]) = (matrix[i, temp + 1], matrix[i, temp]);
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+Main();
